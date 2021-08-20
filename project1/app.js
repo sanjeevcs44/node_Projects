6cea3aa26c3641044524acd806597bb0adc1fc0b
Sanjeev Sharma <sanjeevcs44@gmail.com>
2021-08-20 09:31:52 +0530

    app.js updated version

--- project1/app.js
+++ project1/app.js
@@ -1,9 +1,14 @@
 const http = require('http');
 
+const hostname = '127.0.0.1';
+const port = 3000;
+
 const server = http.createServer((req, res) => {
-    res.end("Your First PRogram");
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
 });
 
-server.listen(4000, () => {
-    console.log("i am on 4000 port");
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
